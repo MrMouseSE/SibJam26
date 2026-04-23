677662f0de1d08a5457eb99635553ee637270bc3
Danil Bogodvid <d.bogodvid@kefirgames.com>
2026-04-23 19:00:05 +0300

    fix: button animations to tweens

--- Assets/Scripts/MainMenuScripts/MenuButtonContainer.cs
+++ Assets/Scripts/MainMenuScripts/MenuButtonContainer.cs
@@ -1,57 +1,74 @@
 using System;
+using System.Threading;
 using SoundsComponentsScripts;
+using TweenScripts;
 using UnityEngine;
 
 namespace MainMenuScripts
 {
     public class MenuButtonContainer : MonoBehaviour
     {
-        public Animation AnimationComponent;
-        public AnimationClip PushClip;
-        public AnimationClip OverClip;
-        public AnimationClip PushUpClip;
+        public float IdleAnimationTime;
+        public TweenGroupAnimation AnimationIdleGroup;
+        public float PushAnimationTime;
+        public TweenGroupAnimation AnimationPushGroup;
 
-        [Space]
-        public AudioContainer ButtonSounds;
-        
+        [Space] public AudioContainer ButtonSounds;
+
         public Action OnButtonPushed;
-        
-        private bool _isButtonPushed = false;
-    
+
+        private bool _isButtonPushed;
+
+        private CancellationTokenSource _animationCancellationToken = new();
+
         private void OnMouseEnter()
         {
-            if(_isButtonPushed) return;
-            AnimationComponent.Play(OverClip.name);
+            if (_isButtonPushed) return;
             ButtonSounds.Play(SoundType.ActionSound);
+            var animationObject = StartUniTaskAnimationProcess(AnimationIdleGroup, IdleAnimationTime, true);
+            animationObject.AnimationCompleted += LoopIdleAnimation;
         }
 
         private void OnMouseExit()
         {
-            if (_isButtonPushed) return;
-            StopAndRewind();
+            _animationCancellationToken?.Cancel();
+            AnimationIdleGroup.SetForceState(true);
         }
 
         private void OnMouseDown()
         {
             _isButtonPushed = true;
-            StopAndRewind();
-            AnimationComponent.Play(PushClip.name);
             ButtonSounds.Play(SoundType.AppearSound);
-            OnButtonPushed?.Invoke();
+            StartUniTaskAnimationProcess(AnimationPushGroup, PushAnimationTime, true);
         }
 
         private void OnMouseUp()
         {
             _isButtonPushed = false;
-            StopAndRewind();
-            AnimationComponent.Play(PushUpClip.name);
             ButtonSounds.Play(SoundType.DeathSound);
+            var animationObject = StartUniTaskAnimationProcess(AnimationPushGroup, PushAnimationTime, false);
+            animationObject.AnimationCompleted += OnPushAnimationCompleted;
         }
 
-        private void StopAndRewind()
+        private void OnPushAnimationCompleted(UniTaskAnimationObject obj)
         {
-            AnimationComponent.Stop();
-            AnimationComponent.Rewind();
+            obj.AnimationCompleted -= OnPushAnimationCompleted;
+            OnButtonPushed?.Invoke();
+        }
+        
+        private UniTaskAnimationObject StartUniTaskAnimationProcess(TweenGroupAnimation animations, float time, bool isForward)
+        {
+            _animationCancellationToken.Cancel();
+            _animationCancellationToken = new CancellationTokenSource();
+            UniTaskAnimationObject uniTaskAnimationObject = new();
+            uniTaskAnimationObject.StartAnimation(animations, _animationCancellationToken.Token, time, isForward)
+                .Forget();
+            return uniTaskAnimationObject;
+        }
+
+        private void LoopIdleAnimation(UniTaskAnimationObject obj)
+        {
+            _ = obj.StartAnimation(AnimationIdleGroup, _animationCancellationToken.Token, IdleAnimationTime, true);
         }
     }
 }

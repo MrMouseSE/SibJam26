094ee6f67f6a7a6177ae98418740aea11a9f13d5
Danil Bogodvid <d.bogodvid@kefirgames.com>
2026-04-25 14:19:27 +0300

    add: new content

--- Assets/Scripts/GlobalMapScripts/GlobalMapSystemContainer.cs
+++ Assets/Scripts/GlobalMapScripts/GlobalMapSystemContainer.cs
@@ -1,16 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class GlobalMapSystemContainer : MonoBehaviour
+namespace GlobalMapScripts
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class GlobalMapSystemContainer : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public List<PointOfInterestContainer> PointOfInterestContainer;
     }
 }
